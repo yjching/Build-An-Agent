2a125c0ee2d21b32e8c919de61636d070d7f5b13
ssayjc <yijian.ching@sas.com>
2025-02-20 18:07:21 +1100

    updates with wikipedia tool

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import re
 import os
 import requests
 import json
+import httpx
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(dotenv_path="Build-An-Agent/.env")
 
@@ -55,21 +56,37 @@ calculate:
 e.g. calculate: 4 * 7 / 3
 Runs a calculation and returns the number - uses Python so be sure to use floating point syntax if necessary
 
-Example session:
+wikipedia:
+e.g. wikipedia: Django
+Returns a summary from searching Wikipedia
 
-Question: What is 4 * 5?
-Thought: I should calculate using the numbers provided. 
-Action: calculate: 4*5
+Example session:
+Question: What is the capital of China?
+Thought: I should look up China on Wikipedia.
+Action: wikipedia: China
 PAUSE
 
 You will be called again with this:
 
-Observation: 4 times 5 is 20.
+Observation: China is a country. The capital is Beijing.
 
 You then output:
 
-Answer: 20.
+Answer: The capital of China is Beijing.
 """.strip()
+
+# Question: What is 4 * 5?
+# Thought: I should calculate using the numbers provided. 
+# Action: calculate: 4*5
+# PAUSE
+
+# You will be called again with this:
+
+# Observation: 4 times 5 is 20.
+
+# You then output:
+
+# Answer: 20.
 
 client = AzureOpenAIClient(
     api_key = os.getenv("azure_openai_key"), 
@@ -79,13 +96,20 @@ client = AzureOpenAIClient(
     system_prompt = system_prompt
 )
 
-question = "What is 5 times 3?"
-
 def calculate(what):
     return eval(what)
 
+def wikipedia(q):
+    return httpx.get("https://en.wikipedia.org/w/api.php", params={
+        "action": "query",
+        "list": "search",
+        "srsearch": q,
+        "format": "json"
+    }).json()["query"]["search"][0]
+
 def run_agent(client, question, no_times):
     known_actions = {
+        "wikipedia": wikipedia,
         "calculate": calculate,
     }
     i = 0
@@ -109,4 +133,49 @@ def run_agent(client, question, no_times):
         else:
             return
 
+question = "5 ** 3"
+run_agent(client, question, 2)
+
+question = "Where is SAS Institute located?"
 run_agent(client, question, 5)
+
+def run_agent_v2(client, question, no_times):
+    known_actions = {
+        "wikipedia": wikipedia,
+        "calculate": calculate,
+    }
+    i = 0
+    # Define the regular expression pattern
+    action_re = re.compile(r'Action:\s*(\w+)\s*:\s*(.*)')
+    next_prompt = question
+    while i < no_times:
+        i+=1
+        result = client.generate_completion(next_prompt)
+        print(i)
+        print(result)
+        # Search for the pattern in the input string
+        actions = [action_re.search(a) for a in result.split('\n') if action_re.search(a)]
+        # Check if a match was found and extract the captured group
+        if actions:
+            action, action_input = actions[0].groups()
+            cleaned_string = action_input.replace("PAUSE", "").strip()
+            observation = known_actions[action](cleaned_string)
+            next_prompt = question+" Observation: {}".format(observation)
+            print(next_prompt)
+        else:
+            return
+        # return
+
+question = "Where is SAS institute?"
+run_agent_v2(client, question, 5)
+
+
+
+def wikipedia_test(q):
+    return httpx.get("https://en.wikipedia.org/w/api.php", params={
+        "action": "query",
+        "list": "search",
+        "srsearch": q,
+        "format": "json"
+    }).json()
+wikipedia_test("SAS Institute")
