8e38d811b1e10479227609fd557796965b4a5c11
ssayjc <yijian.ching@sas.com>
2025-02-21 17:50:30 +1100

    reflection agentic loop

--- main.py
+++ main.py
@@ -7,39 +7,6 @@ from clients.azure_openai import AzureOpenAIClient
 from tools.main import calculate, wikipedia
 # from clients.ollama import OllamaClient
 
-system_prompt = """
-You run in a loop of Thought, Action, PAUSE, Observation.
-At the end of the loop you output an Answer
-Use Thought to describe your thoughts about the question you have been asked.
-Use Action to run one of the actions available to you - then return PAUSE.
-Observation will be the result of running those actions.
-
-Your available actions are:
-
-calculate:
-e.g. calculate: 4 * 7 / 3
-Runs a calculation and returns the number - uses Python so be sure to use floating point syntax if necessary
-
-wikipedia:
-e.g. wikipedia: Django
-Returns a summary from searching Wikipedia
-
-Example session:
-Question: What is the capital of China?
-Thought: I should look up China on Wikipedia.
-Action: wikipedia: China
-PAUSE
-
-You will be called again with this:
-
-Observation: China is a country. The capital is Beijing.
-
-You then output:
-
-Answer: The capital of China is Beijing.
-""".strip()
-
-
 base_client = AzureOpenAIClient(
     api_key = os.getenv("azure_openai_key"), 
     endpoint = "ssayjc.openai.azure.com",
@@ -55,28 +22,7 @@ eval_client = AzureOpenAIClient(
         system_prompt=""
 )
 
-def run_reflection(client, eval_client, question, n_iter=10):
-    eval_client.system_prompt = f"""Evaluate how good the following response is, given the question. If the response ise poor, respond with your evaluation on what to improve with the response.
-    If the response is good and does not need changes, append the following to the end of your response: <OK>. 
-    Question: {question}.
-    Response: """
-    step = 0
-    while step < n_iter:
-        step+=1
-        response = client.generate_completion(question)
-        print(f"Responder: My initial response is: {response}")
-        print(f"Responder: I will send this to the evaluator")
-        ## Save to client memory
-        evaluation_of_response = eval_client.generate_completion(response)
-        print(f"Evaluator: My evaluation is {evaluation_of_response}")
-        ## Save to evaluation memory
-        if "<OK>" in evaluation_of_response:
-            print("Stop reflection loop.")
-            break
-        else: 
-            question = response
-    return response
+from strategies.main import ReflectionStrategy
 
-
-run_reflection(base_client, eval_client, "Other than knock knock jokes, what else did I ask related to Camille?")
-
+agent = ReflectionStrategy(base_client, eval_client)
+agent.run("Which knock knock joke that you wrote for Camille is your favourite?")
--- strategies/main.py
+++ strategies/main.py
@@ -1,8 +1,9 @@
 from clients.main import BaseClient
 
 class ReflectionStrategy():
-    def __init__(self, clients: dict[BaseClient, BaseClient]):
-        self._clients = clients
+    def __init__(self, base_client, eval_client):
+        self.base_client = base_client
+        self.eval_client = eval_client
     @property
     def clients(self):
         return self._clients
@@ -10,3 +11,25 @@ class ReflectionStrategy():
     @clients.setter
     def clients(self, value):
         self._clients = value
+    
+    def run(self, question, n_iter: int = 10):
+        self.eval_client.system_prompt = f"""Evaluate how good the following response is, given the question. If the response ise poor, respond with your evaluation on what to improve with the response.
+        If the response is good and does not need changes, append the following to the end of your response: <OK>. 
+        Question: {question}.
+        Response: """
+        step = 0
+        while step < n_iter:
+            step+=1
+            response = self.base_client.generate_completion(question)
+            print(f"Responder: My initial response is: {response}")
+            print(f"Responder: I will send this to the evaluator")
+            ## Save to client memory
+            evaluation_of_response = self.eval_client.generate_completion(response)
+            print(f"Evaluator: My evaluation is {evaluation_of_response}")
+            ## Save to evaluation memory
+            if "<OK>" in evaluation_of_response:
+                print("Stop reflection loop.")
+                break
+            else: 
+                question = response
+        return response
